61bdd09576b55ac503f65f6dfce87d947b1c5725
alfarissy040 <alfarissy040@gmail.com>
2022-11-29 21:25:18 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kelompok_4/Helpers/UserInfo.dart';
 import 'package:kelompok_4/Ui/LoginPage.dart';
 import 'package:kelompok_4/Ui/ProdukPage.dart';
 
@@ -6,19 +7,40 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget page = CircularProgressIndicator();
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin();
+  }
+
+  void isLogin() async {
+    var token = await UserInfo().getToken();
+    if (token!.isNotEmpty) {
+      setState(() {
+        page = ProdukPage();
+      });
+    } else {
+      setState(() {
+        page = LoginPage();
+      });
+    }
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Toko kita',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
+      title: "Toko Kita",
       debugShowCheckedModeBanner: false,
-      home: ProdukPage(),
+      home: page,
     );
   }
 }
