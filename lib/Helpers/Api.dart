2dc327909fb940f804252033baff4ab12a7e609e
alfarissy040 <alfarissy040@gmail.com>
2022-11-29 20:04:12 +0700

    menupdate APi,ApiUrl

---
+++ lib/Helpers/Api.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:kelompok_4/Helpers/AppException.dart';
+import 'package:kelompok_4/Helpers/UserInfo.dart';
+import "package:http/http.dart" as http;
+
+class Api {
+  Future<dynamic> post(String url, dynamic data) async {
+    var token = await UserInfo().getToken();
+    var responseJson;
+    try {
+      final response = await http.post(Uri.parse(url),
+          body: data,
+          headers: {HttpHeaders.authorizationHeader: "Bearer $token"});
+      responseJson = ReturnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No Internet connection");
+    }
+    return responseJson;
+  }
+
+  Future<dynamic> get(String url) async {
+    var token = await UserInfo().getToken();
+    var responseJson;
+    try {
+      final response = await http.get(Uri.parse(url),
+          headers: {HttpHeaders.authorizationHeader: "Bearer $token"});
+      responseJson = ReturnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No Internet connection");
+    }
+    return responseJson;
+  }
+
+  Future<dynamic> delete(String url) async {
+    var token = await UserInfo().getToken();
+    var responseJson;
+    try {
+      final response = await http.delete(Uri.parse(url),
+          headers: {HttpHeaders.authorizationHeader: "Bearer $token"});
+      responseJson = ReturnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No Internet connection");
+    }
+    return responseJson;
+  }
+
+  dynamic ReturnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return response;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 422:
+        throw InvalidInputException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            "Error occured while Communication with server with status code: ${response.statusCode}");
+    }
+  }
+}
